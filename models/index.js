0f5f56f7621400c376dac47b1beb8d08a1c4b2df
elizabeth <elizabethannabraham@outlook.com>
2021-07-23 11:37:31 -0500

    Create Item model and testing

--- models/index.js
+++ models/index.js
@@ -2,14 +2,17 @@ const {Sequelize, DataTypes, Model} = require('sequelize')
 const {sequelize} = require('../db')
 
 
-class Sauce extends Model {} //change to Inventory models
+class Item extends Model {} //change to Inventory models
 
-Sauce.init({                 //change to Inventory models
-    name: DataTypes.STRING,
+Item.init({                 //change to Inventory models
+    title: DataTypes.STRING,
+    price: DataTypes.FLOAT(2),
+    description: DataTypes.TEXT,
+    category: DataTypes.STRING,
     image: DataTypes.STRING,
 }, {
     sequelize,
     timestamps: false,
 });
 
-module.exports = {Sauce}; //change to export Inventory models
+module.exports = {Item}; //change to export Inventory models
--- models/index.test.js
+++ models/index.test.js
@@ -1,14 +1,19 @@
-const {Sauce} = require('./index'); //change to Inventory models
+const {Item} = require('./index'); //change to Inventory models
 const {sequelize} = require('../db');
 
-describe('Sauce Model', () => {		//change test to Inventory models
+describe('Item Model', () => {		//change test to Inventory models
 	beforeAll(async () => {
 		await sequelize.sync({force: true})
 	});
 
-	test('can create a Sauce', async() => {
-		const testSauce = await Sauce.create({name : 'Cholula'})
-		expect(testSauce.name).toBe('Cholula')
+	test('can create a Item', async() => {
+		const testItem = await Item.create({title : 'Red Tie', price: 68.90, description: 'Stylish tie', category: 'men\'s clothing', image: 'url.png'})
+		console.log(testItem.category)
+		expect(testItem.title).toBe('Red Tie') 
+		expect(testItem.price).toBe(68.90)
+		expect(testItem.description).toBe('Stylish tie')
+		expect(testItem.category).toBe('men\'s clothing')
+		expect(testItem.image).toBe('url.png')
 	})
 
 
