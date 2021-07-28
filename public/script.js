e8febaace77ba20df3bab92e3661ab5f023ad1e1
RSalcido2019 <48775450+RSalcido2019@users.noreply.github.com>
2021-07-28 10:02:42 -0500

    Changes to the layout on single items/ create new items form/ and antony's nav color change

--- public/script.js
+++ public/script.js
@@ -19,3 +19,20 @@ function backToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
